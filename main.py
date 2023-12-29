76e224ea983b92a1f8a43cd77c59f5af6580fb4c
vcdfh23 <s2310710003@fhooe.at>
2023-12-29 09:28:04 +0100

    First calc. Problems with parser occured -> need to be fixed

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ __doc__ = "sample file for a main methode"
 #import system libs
 import argparse
 import sys
-#from scipy import constants
-
-
+from scipy.constants import g
+import numpy as np
 
 #own modules
 from examples.plot_example import exec_sample_plot_
 
-#Fricition coefficents
+#Fricition coefficents from Powerpoint (constants)
 concrete_dry = 0.5
 concrete_wet = 0.35
 ice_dry = 0.15
@@ -27,22 +26,50 @@ print (cmd_call_args_.pdf_file_out)
 
 ### parameter parser for: mass, velocity, road type, wet&dry, inclination
 parameter_parser = argparse.ArgumentParser(description='Variance Parameter for calculation')
-parameter_parser.add('mass',type=int,help='Mass of the vehicle.')
-parameter_parser.add('velocity',type=int,help='Velocity of the vehicle.')
-parameter_parser.add('road_type',type=str,help='Road type')
-parameter_parser.add('road_condition',type=str,help='Road condition wet or dry.')
-parameter_parser.add('inclination',type=int,help='Road inclination (optional).')
-param_pars_args = parameter_parser.parse_args()
+parameter_parser.add_argument('mass',type=float,default=0,help='Mass of the vehicle.')
+parameter_parser.add_argument('velocity',type=float,default=28,help='Velocity of the vehicle.')
+parameter_parser.add_argument('road_type',type=str,default='concrete',choices = ['concrete','ice','gravel','sand'],help='Road type')
+parameter_parser.add_argument('road_condition',type=str,default='dry',choices = ['dry','wet'],help='Road condition wet or dry.')
+parameter_parser.add_argument('inclination',type=int,default=0,help='Road inclination optional.')
+#param_pars_args = parameter_parser.parse_args() # ERROR in this line!
 
+#ToDO: Fix param_pars_args and change function values in method calc_decelleration
+def calc_decelleration(mass=10, velocity=28, road_type='concrete', road_condition='dry', inclination=0):
+  
+  if road_type == 'concrete' and 'dry':
+    road_µ = concrete_dry
+  elif road_type == 'concrete' and 'wet':
+    road_µ = concrete_wet
+  elif road_type == 'ice' and 'dry':
+    road_µ = ice_dry
+  elif road_type == 'ice' and 'wet':
+    road_µ = ice_wet
+  elif road_type == 'gravel' and 'dry':
+    road_µ = gravel_dry
+  elif road_type == 'sand' and 'dry':
+    road_µ = sand_dry
+  else: 
+    print("Wrong input!")
 
-
+  #if inclination == 0:
+  
+  #Calculation of timevector with a=g*µ and t = v/a
+  accel = g*road_μ
+  stop_time = velocity/accel
+  time_vector = np.arange(0,stop_time+0.1,0.1) #from https://numpy.org/doc/stable/reference/generated/numpy.arange.html
+  
+  #Calculation of distance and velocity vector!
+  for i in time_vector:
+    calc_vel = velocity-accel*i
+    calc_s_stop = calc_vel*i+0.5*accel*i**2
+    print(calc_vel)
 
 
 #===============
 # a method
 def main_method():
   
-
+  calc_decelleration()
   ###
   ### Code from Prof. Altinger
   main_method.__doc__ = "sample main method"
@@ -51,6 +78,5 @@ def main_method():
 #===============
 # do work and call a methode
 main_method()
-
 #terminate program
 sys.exit()
