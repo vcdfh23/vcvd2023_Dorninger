Calculated Rule of thumb; Made Plots for correct calculation, changed json config

main.py
@@ -7,7 +7,7 @@ from scipy.constants import g
 import numpy as np
 
 #own modules
-from examples.plot_example import exec_sample_plot_
+from examples.plot_example import exec_sample_plot_,plot_correct_calc
 
 #Fricition coefficents from Powerpoint (constants)
 concrete_dry = 0.5
@@ -30,6 +30,21 @@ param_pars_args = parameter_parser.parse_args()
 
 print (param_pars_args.pdf_file_out)
 
+#Rule of Thumb Calculation:
+#ToDo: Calculate time for sstop and sstop_danger
+#      Plot them and give them to the pdf Report
+def rule_of_thumb(velocity= float(param_pars_args.velocity)):
+  velocity = velocity*3.6 # conversion from m/s in km/h needed for RoT
+  snormal = (velocity/10)**2
+  sdanger = snormal*0.5
+  sreaction = (velocity/10)*3
+  sstop = snormal + sreaction
+  sstop_danger = sdanger + sreaction
+
+  #print(sstop)
+  #print(sstop_danger)
+  return(sstop,sstop_danger)
+#Proper Calculation 
 def calc_decelleration(mass=param_pars_args.mass, velocity=float(param_pars_args.velocity), road_type=param_pars_args.road_type
                        , road_condition=param_pars_args.road_condition, inclination=float(param_pars_args.inclination)):
   
@@ -54,23 +69,34 @@ def calc_decelleration(mass=param_pars_args.mass, velocity=float(param_pars_args
   accel = g*road_μ
   stop_time = velocity/accel
   time_vector = np.arange(0,stop_time+0.1,0.1) #from https://numpy.org/doc/stable/reference/generated/numpy.arange.html
-  
+  calc_vel_array = []
+  calc_s_stop_array = []
+
   #Calculation of distance and velocity vector!
   for i in time_vector:
     calc_vel = velocity-accel*i
     calc_s_stop = calc_vel*i+0.5*accel*i**2
-    print(calc_vel)
+    
+    #Store all values in arrays:
+    calc_vel_array.append(calc_vel)
+    calc_s_stop_array.append(calc_s_stop)
+    #print(calc_vel)
 
-
+  #print(calc_s_stop)
+  return (calc_vel_array,calc_s_stop_array,time_vector)
 #===============
 # a method
 def main_method():
   
-  calc_decelleration()
+  calc_v, calc_s,t_vector = calc_decelleration()
   ###
   ### Code from Prof. Altinger
   main_method.__doc__ = "sample main method"
   exec_sample_plot_(param_pars_args.pdf_file_out)
+  plot_correct_calc(calc_v, calc_s,t_vector)
+  #rule_of_thumb()
+  #calc_decelleration()
+
 
 #===============
 # do work and call a methode