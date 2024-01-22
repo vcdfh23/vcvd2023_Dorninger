0dfb9215013366befb22fcbe54921798aabd49e5
vcdfh23 <s2310710003@fhooe.at>
2024-01-22 18:29:24 +0100

    Google coding guideline implemented!

--- examples/plot_example.py
+++ examples/plot_example.py
@@ -3,15 +3,16 @@ __doc__ = "sample file for ploting method \
            https://www.geeksforgeeks.org/matplotlib-figure-figure-add_axes-in-python/"
 
 #based on
-#source: https://www.geeksforgeeks.org/matplotlib-figure-figure-add_axes-in-python/
+#src: https://www.geeksforgeeks.org/matplotlib-figure-figure-add_axes-in-python/
 
 #required imports
 import matplotlib.pyplot as plt
-import numpy as np
 
 #Plot exact calculation and Rule of Thumb in one PDF-Document:
-
-def plot_calculation(calc_v, calc_s,t_vector,sstop_vector,sstop_danger_vector,t_sstop,t_sstop_danger):
+def plot_calculation(calc_v, calc_s,
+                     t_vector,sstop_vector,
+                     sstop_danger_vector,
+                     t_sstop,t_sstop_danger):
   fig,ax = plt.subplots(1,4,figsize=(18,5))
 
   ax[0].plot(t_vector,calc_v)
@@ -23,16 +24,13 @@ def plot_calculation(calc_v, calc_s,t_vector,sstop_vector,sstop_danger_vector,t_
   ax[1].set_xlabel("Time [s]")
   ax[1].set_ylabel("Distance [m]")
   ax[1].set_title("Distance \n", fontweight ="bold")
-
   ax[2].plot(t_sstop,sstop_vector)
   ax[2].set_xlabel("Time[s]")
   ax[2].set_ylabel("Distance [m] (sstop)")
   ax[2].set_title("sstop (Rule of thumb)\n", fontweight ="bold")
-
   ax[3].plot(t_sstop_danger,sstop_danger_vector)
   ax[3].set_xlabel("Time [s]")
   ax[3].set_ylabel("Distance [m] (sstop_danger)")
-  ax[3].set_title("sstop_danger (Rule of thumb) \n", fontweight ="bold") 
-  
+  ax[3].set_title("sstop_danger (Rule of thumb) \n", fontweight ="bold")
   plt.savefig("s2310710003.pdf")
   print("s2310710003.pdf has been built!")
--- s2310710003.py
+++ s2310710003.py
@@ -1,7 +1,9 @@
+__doc__ = 'Calculation for braking distance!'
 #Run script with F5 for default values from json file
 #Individual parameters example:
-# & C:/Users/geral/AppData/Local/Programs/Python/Python310/python.exe 
-# c:/Users/geral/VCVD/vcvd2023_Dorninger/s2310710003.py --mass="1000" --inclination="-5" 28 concrete dry
+# & C:/Users/geral/AppData/Local/Programs/Python/Python310/python.exe
+# c:/Users/geral/VCVD/vcvd2023_Dorninger/s2310710003.py
+# --mass="1000" --inclination="-5" 28 concrete dry
 
 #import Libraries
 import argparse
@@ -20,13 +22,13 @@ gravel_dry = 0.35
 sand_dry = 0.3
 
 ### parameter parser for: mass, velocity, road type, wet&dry, inclination
-parameter_parser = argparse.ArgumentParser(description='Variance Parameter for calculation')
-parameter_parser.add_argument('--mass',type=str,help='Mass of the vehicle.')
-parameter_parser.add_argument('velocity',type=str, help='Velocity of the vehicle.')
-parameter_parser.add_argument('road_type',type=str,help='Road type') #,choices = ['concrete','ice','gravel','sand'],
-parameter_parser.add_argument('road_condition',type=str,help='Road condition wet or dry.')#choices = ['dry','wet'],
-parameter_parser.add_argument('--inclination',type=str,help='Road inclination optional.')
-param_pars_args = parameter_parser.parse_args() 
+parameter_parser = argparse.ArgumentParser(description='Calculation parameter')
+parameter_parser.add_argument('--mass',type=str,help='Mass of the vehicle')
+parameter_parser.add_argument('velocity',type=str, help='Vehicle velocity')
+parameter_parser.add_argument('road_type',type=str,help='Road type')
+parameter_parser.add_argument('road_condition',type=str,help='wet or dry')
+parameter_parser.add_argument('--inclination',type=str,help='inclination')
+param_pars_args = parameter_parser.parse_args()
 print(param_pars_args)
 
 # If optional parameters are None -> assign standard values
@@ -47,45 +49,47 @@ def rule_of_thumb(velocity= float(param_pars_args.velocity)):
   sstop_danger = sdanger + sreaction
   sstop_vector = np.arange(0,sstop+0.1,0.1)
   sstop_danger_vector = np.arange(0,sstop_danger+0.1,0.1)
-  
   #Time calculation and vector-generation
-  t_sstop_danger = (sstop_danger_vector / (velocity/kmh_to_ms))
-  t_sstop = (sstop_vector / (velocity/kmh_to_ms))
-  
+  t_sstop_danger = sstop_danger_vector / (velocity/kmh_to_ms)
+  t_sstop = sstop_vector / (velocity/kmh_to_ms)
   return(sstop_vector,sstop_danger_vector,t_sstop,t_sstop_danger)
 
-#Precise Calculation of breaking distance:
-def calc_decelleration(mass=float(param_pars_args.mass), velocity=float(param_pars_args.velocity), road_type=param_pars_args.road_type
-                       , road_condition=param_pars_args.road_condition, inclination=float(param_pars_args.inclination)):
-  
+#Precise Calculation of braking distance:
+def calc_decelleration(velocity=float(param_pars_args.velocity),
+                       road_type=param_pars_args.road_type,
+                       road_condition=param_pars_args.road_condition,
+                       inclination=float(param_pars_args.inclination)):
+
   #Calculate degrees in radians:
   #from https://numpy.org/doc/stable/reference/generated/numpy.deg2rad.html
   inclination = np.deg2rad(inclination)
 
-  if road_type == 'concrete' and 'dry':
-    road_µ = concrete_dry
-  elif road_type == 'concrete' and 'wet':
-    road_µ = concrete_wet
-  elif road_type == 'ice' and 'dry':
-    road_µ = ice_dry
-  elif road_type == 'ice' and 'wet':
-    road_µ = ice_wet
-  elif road_type == 'gravel' and 'dry':
-    road_µ = gravel_dry
-  elif road_type == 'sand' and 'dry':
-    road_µ = sand_dry
-  else: 
-    print("Wrong input!")
+  if road_type == 'concrete' and road_condition == 'dry':
+    road_coeff = concrete_dry
+  elif road_type == 'concrete' and road_condition == 'wet':
+    road_coeff = concrete_wet
+  elif road_type == 'ice' and road_condition == 'dry':
+    road_coeff = ice_dry
+  elif road_type == 'ice' and road_condition == 'wet':
+    road_coeff = ice_wet
+  elif road_type == 'gravel' and road_condition == 'dry':
+    road_coeff = gravel_dry
+  elif road_type == 'sand' and road_condition == 'dry':
+    road_coeff = sand_dry
+  else:
+    print('Wrong input!')
 
   if inclination == 0:
-    accel = g*road_μ
+    accel = g*road_coeff
   else:
     #calculation with inclination:
-    accel = g*road_μ*np.cos(inclination)+g*np.sin(inclination)
-    
+    accel = g*road_coeff*np.cos(inclination)+g*np.sin(inclination)
+
   #Calculation of timevector with a=g*µ and t = v/a
   stop_time = velocity/accel
-  time_vector = np.arange(0,stop_time+0.1,0.1) #from https://numpy.org/doc/stable/reference/generated/numpy.arange.html
+  #from https://numpy.org/doc/stable/reference/generated/numpy.arange.html
+  time_vector = np.arange(0,stop_time+0.1,0.1)
+  ###
   calc_vel_array = []
   calc_s_stop_array = []
 
@@ -97,19 +101,16 @@ def calc_decelleration(mass=float(param_pars_args.mass), velocity=float(param_pa
     #Store all values in arrays:
     calc_vel_array.append(calc_vel)
     calc_s_stop_array.append(calc_s_stop)
-  
+
   return (calc_vel_array,calc_s_stop_array,time_vector)
 
 
 #===============
-# a method
+# main method
 def main_method():
   sstop_vector,sstop_danger_vector,t_sstop,t_sstop_danger = rule_of_thumb()
   calc_v, calc_s,t_vector = calc_decelleration()
-  plot_calculation(calc_v, calc_s,t_vector,sstop_vector,sstop_danger_vector,t_sstop,t_sstop_danger)
-  
-#===============
-# do work and call a methode
+  plot_calculation(calc_v, calc_s,t_vector,sstop_vector,
+                  sstop_danger_vector,t_sstop,t_sstop_danger)
 main_method()
-#terminate program
 sys.exit()
